09fb5cf5eb73f38f3a8be61035bba4989226e5f9
Noemi Tigasi <noemitigasi1897@gmail.com>
2022-01-25 16:21:43 -0500

    Insertado altas, bajas y cambios en php y mysql

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+$servidor="localhost";
+$usuario="rosas";
+$clave="Noemi123tigasi";
+$base="registro";
+$tabla="pedido";
+
+$codigo-$_POST['codigo'];
+$nombre-$_POST['nombre'];
+$correo-$_POST['correo'];
+$cantidad-$_POST['cantidad'];
+
+$conexion=mysqli_connect($servidor,$usuario,$clave) or die ("error en la conexion del servidor");
+$bases=mysqli_select_db($conexion,$base) or die ("error al conectarse con la base de datos");
+$insertar="insert into pedido values('$codigo','$nombre','$correo','$cantidad')";
+$resultado=mysqli_query($conexion,$insertar) or die ("error al conectar con ta tabla");
+mysqli_close($conexion);
+echo("Los datos se han guardado exitosamente");
+?>
