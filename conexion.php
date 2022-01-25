86d98fb3bca74eadd6e30212035b1f33b9aa7bbf
unknown <wilsonjavierchimbamartinez@gmail.com>
2022-01-25 18:36:27 -0500

    Modificacion del codigo de la conexion a la base de datos

--- conexion.php
+++ conexion.php
@@ -12,7 +12,7 @@ $cantidad=$_POST['cantidad'];
 
 $conexion=mysqli_connect($servidor,$usuario,$clave) or die ("error en la conexion del servidor");
 $bases=mysqli_select_db($conexion,$base) or die ("error al conectarse con la base de datos");
-$insertar="insert into pedido value('$codigo','$nombre','$correo','$cantidad')";
+$insertar="insert into pedido values('$codigo','$nombre','$correo','$cantidad')";
 $resultado=mysqli_query($conexion,$insertar) or die ("error al conectar con ta tabla");
 mysqli_close($conexion);
 echo("Los datos se han guardado exitosamente");
